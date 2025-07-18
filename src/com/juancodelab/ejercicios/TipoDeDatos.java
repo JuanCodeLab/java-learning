3d4c1b397612ff3a148f2a624fc09870024192a2
Juan Carlos <juan.carlos.nuno@outlook.com>
2025-07-17 21:57:37 -0600

    Reconecto el proyecto desde la nueva ubicacion

---
+++ src/com/juancodelab/ejercicios/TipoDeDatos.java
@@ -0,0 +1,40 @@
+package com.juancodelab.ejercicios;
+
+public class TipoDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		byte edadbyte = 22;
+		short year = 2025;
+		int id = 4587654;
+		long horas = 99222288;
+		
+		float estatura = 1.80F;
+		double resultado = 932.4569854;
+		
+		char genero = 'F'; //Femenino
+		
+		boolean status = true;
+		boolean existe = false;
+		
+		
+		
+		System.out.println("Tipos de dato\n");
+		System.out.println("byte  i = 0; // 1 byte  -->  ejemplo byte edad = " + edadbyte);
+		System.out.println("short j = 0; // 2 byte  -->  ejemplo short year = " + year);
+		System.out.println("int   k = 0; // 4 byte  -->  ejemplo id = " + id);
+		System.out.println("long  l = 0; // 8 byte  -->  ejemplo horas = " + horas);
+		
+		System.out.println("\n\nOtros tipos de dato\n");
+		System.out.println("float  = 0; // 8 byte  -->  ejemplo horas = " + horas);
+		System.out.println("long  l = 0; // 8 byte  -->  ejemplo horas = " + horas);
+		System.out.println("long  l = 0; // 8 byte  -->  ejemplo horas = " + horas);
+		System.out.println("long  l = 0; // 8 byte  -->  ejemplo horas = " + horas);
+		
+		
+		
+	}
+
+}
